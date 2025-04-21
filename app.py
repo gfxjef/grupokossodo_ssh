cd427a6b31881d41791159d1e4f4b9605edc409f
gfxjef <gfxjef@gmail.com>
2025-04-21 14:17:30 -0500

    agregamos todo

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = create_app()
 
 if __name__ == '__main__':
     # Obtener el puerto del entorno o usar 5000 por defecto
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     
     # Ejecutar la aplicación en modo debug durante el desarrollo
     app.run(host='0.0.0.0', port=port, debug=True)
